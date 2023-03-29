9b77ec153f7d83dc2477b57e51e60c98b2eefe3e
Abdullah-Al-Nahian <nahianiiuc@gmail.com>
2023-03-29 12:55:35 +0600

    note item card

--- src/contexts/notes/NoteSate.js
+++ src/contexts/notes/NoteSate.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    
+    const notesInitial = [
+        {
+          "_id": "641be5cf5ad40ece02035e8c",
+          "user": "64194e8fd133e822b91fcb16",
+          "title": "Alarm",
+          "description": "Wake up at 5:30",
+          "tag": "personal",
+          "date": "2023-03-23T05:38:23.169Z",
+          "__v": 0
+        },
+        {
+          "_id": "6423d41b03f91ef8d1436535",
+          "user": "64194e8fd133e822b91fcb16",
+          "title": "Alarm 2",
+          "description": "Wake up at 6:30",
+          "tag": "personal",
+          "date": "2023-03-29T06:00:59.753Z",
+          "__v": 0
+        }
+      ]
+    const [notes, setNotes] = useState(notesInitial);
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
